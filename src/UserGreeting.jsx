9b6a6c2fd761cebd08129d843bddc071a559e35c
Mattheus-Costa <costamattheus4@gmail.com>
2026-08-19 10:30:41 -0300

    Revert "mudanças no css e mensagem de boas-vindas"
    This reverts commit 17832a8f75db8b0c9aa7425c792dcc7ba2ef0699.

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,8 +1,7 @@
 function UserGreeting({isLoggedIn = false,username = "Guest"}){
 
     const WelcomeGreeting = <div  className="welcome-greeting" >
-        <h2>Bem vindo CUZÃO, {username} </h2>
-        
+        <h2>Bem vindo, {username} </h2>
     </div>
 
     const LoginRequest = <div className="login-request">
